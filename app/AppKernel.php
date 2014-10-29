8d7d60ae76d16d55cd824eb431507287b87d4606
Zander Baldwin <mynameiszanders@gmail.com>
2014-10-29 13:19:39 +0000

    Update Application Kernel
    Update coding style, and correct the instantiation of the main application bundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,36 +1,51 @@
 <?php
 
-use Symfony\Component\HttpKernel\Kernel;
-use Symfony\Component\Config\Loader\LoaderInterface;
+    use Symfony\Component\HttpKernel\Kernel;
+    use Symfony\Component\Config\Loader\LoaderInterface;
 
-class AppKernel extends Kernel
-{
-    public function registerBundles()
+    class AppKernel extends Kernel
     {
-        $bundles = array(
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Acme\HelloBundle\AcmeHelloBundle()
-        );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+        /**
+         * Register Bundles
+         *
+         * @access public
+         * @return Symfony\Component\HttpKernel\Bundle\Bundle[]
+         */
+        public function registerBundles()
+        {
+            $bundles = [
+                new Symfony\Bundle\FrameworkBundle\FrameworkBundle,
+                new Symfony\Bundle\TwigBundle\TwigBundle,
+                new Symfony\Bundle\MonologBundle\MonologBundle,
+                new AppBundle\AppBundle,
+            ];
+
+            if(in_array($this->getEnvironment(), ['dev', 'test'])) {
+                $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle;
+            }
+
+            return $bundles;
         }
 
-        return $bundles;
-    }
+        /**
+         * Register Container Configuration
+         *
+         * @access public
+         * @param Symfony\Component\Config\Loader\LoaderInterface $loader
+         * @return void
+         */
+        public function registerContainerConfiguration(LoaderInterface $loader)
+        {
+            $loader->load(__DIR__ . '/config/config.yml');
 
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $loader->load(__DIR__ . '/config/config.yml');
-
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $loader->load(function ($container) {
-                $container->loadFromExtension('web_profiler', array(
-                    'toolbar' => true,
-                ));
-            });
+            if(in_array($this->getEnvironment(), ['dev', 'test'])) {
+                $loader->load(function ($container) {
+                    $container->loadFromExtension('web_profiler', [
+                        'toolbar' => true,
+                    ]);
+                });
+            }
         }
+
     }
-}
